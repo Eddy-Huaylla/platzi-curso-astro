cf6e8927484d4eedd085f65242ddabc45d928537
Eddy Huaylla <ehuaylla@attachconsultores.com>
2025-08-26 01:19:08 -0500

    feat(project): scaffold initial Astro project and development environment
    - Initialize project using the Astro "basics" template.
    - Configure a development container with Docker, Nginx, and PNPM.
    - Add VSCode settings for recommended extensions and debugging.
    - Set up a simplified Nginx configuration for local SSL proxy.
    - Include standard project files like .gitignore and .editorconfig.
    - Regenerate local SSL certificates.

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	vite: {
+		server: {
+			host: '0.0.0.0',
+			port: 4321,
+			allowedHosts: [
+				'dev.curso-astro.com',
+			],
+		},
+	},
+});
