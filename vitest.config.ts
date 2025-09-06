ce5358efbbeebfec7a72f65c69df94af6df74011
Eddy Huaylla <ehuaylla@attachconsultores.com>
2025-09-06 04:13:15 -0500

    feat: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { getViteConfig } from "astro/config";
+
+export default getViteConfig({
+	test: {
+		// Vitest configuration options can go here.
+		// For example, to define test-related globals:
+		// globals: true,
+	},
+});
