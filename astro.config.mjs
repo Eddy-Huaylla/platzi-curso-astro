69341e2a75645ed64cd3ac53b25a5eed5f4f201b
Eddy Huaylla <ehuaylla@attachconsultores.com>
2025-09-07 08:24:03 -0500

    feat(react): add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind'
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		server: {
-			host: '0.0.0.0',
-			port: 4321,
-			allowedHosts: [
-				'dev.curso-astro.com',
-			],
-		},
-	},
-	integrations: [
-		tailwind(),
-	],
+    vite: {
+        server: {
+            host: '0.0.0.0',
+            port: 4321,
+            allowedHosts: [
+                'dev.curso-astro.com',
+            ],
+        },
+    },
+    integrations: [tailwind(), react()],
 });
